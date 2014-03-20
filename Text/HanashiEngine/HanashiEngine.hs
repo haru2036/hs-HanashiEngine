8a267e4a45af7a04377fd9e3da333a8af579e2ee
haru2036 <haru2036@gmail.com>
2014-03-20 21:27:05 +0900

    added process of counting trigram

--- Text/HanashiEngine/HanashiEngine.hs
+++ Text/HanashiEngine/HanashiEngine.hs
@@ -1,6 +1,14 @@
 --import Text.HanashiEngine.MeCabTools as MecabTools
 --import Text.HanashiEngine.IO as HIO
+import Data.Map
+import Data.List
 
 makeTrigramList :: [String] -> [[String]]
 makeTrigramList all@[a, b, c] = [all]
 makeTrigramList all = (take 3 all) : (makeTrigramList (tail all))
+
+searchAndCountWords :: [String] -> [[String]] -> Int
+searchAndCountWords key items = length $ Data.List.filter (==key) items
+
+getTrigramFrequency :: [[String]] -> Map [String] Int
+getTrigramFrequency sList = fromList [(a, searchAndCountWords a sList) | a <- nub sList]
