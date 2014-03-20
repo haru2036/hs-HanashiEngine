947317bc3e4878ba8dbd50b497f1ba5265985874
haru2036 <haru2036@gmail.com>
2014-03-21 07:14:52 +0900

    Renamed file and added some functions

--- Text/HanashiEngine/Train.hs
+++ Text/HanashiEngine/Train.hs
@@ -1,14 +1,20 @@
+module Text.HanashiEngine.Train
+( makeTrigramList
+)where
 --import Text.HanashiEngine.MeCabTools as MecabTools
 --import Text.HanashiEngine.IO as HIO
 import Data.Map
 import Data.List
+makeTrigramFreqList :: [String] -> Map [String] Int
+makeTrigramFreqList strings = getTrigramFrequency $ makeTrigramList strings
 
 makeTrigramList :: [String] -> [[String]]
-makeTrigramList all@[a, b, c] = [all]
-makeTrigramList all = (take 3 all) : (makeTrigramList (tail all))
+makeTrigramList listAll@[a, b, c] = [listAll]
+makeTrigramList listAll = (take 3 listAll) : (makeTrigramList (tail listAll))
 
 searchAndCountWords :: [String] -> [[String]] -> Int
 searchAndCountWords key items = length $ Data.List.filter (==key) items
 
 getTrigramFrequency :: [[String]] -> Map [String] Int
 getTrigramFrequency sList = fromList [(a, searchAndCountWords a sList) | a <- nub sList]
+
