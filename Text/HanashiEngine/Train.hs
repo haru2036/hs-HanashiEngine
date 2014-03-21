b3463cd9c10556883de30b5649093ae36628d0b4
haru2036 <haru2036@gmail.com>
2014-03-21 20:32:23 +0900

    Added process of make trigram model

--- Text/HanashiEngine/Train.hs
+++ Text/HanashiEngine/Train.hs
@@ -3,18 +3,27 @@ module Text.HanashiEngine.Train
 )where
 --import Text.HanashiEngine.MeCabTools as MecabTools
 --import Text.HanashiEngine.IO as HIO
-import Data.Map
-import Data.List
-makeTrigramFreqList :: [String] -> Map [String] Int
+import Data.Map as Map
+import Data.List as List
+type TrigramModel = Map String (Map String (Map String Int))
+
+makeTrigramFreqList :: [String] -> [([String], Int)]
 makeTrigramFreqList strings = getTrigramFrequency $ makeTrigramList strings
+
+makeTrigramModel :: [([String], Int)] -> TrigramModel
+makeTrigramModel [(a, count)] = makeTrigramItem (a, count)
+makeTrigramModel listAll = Map.union (makeTrigramItem (head listAll)) (makeTrigramModel (tail listAll))
+
+makeTrigramItem :: ([String], Int) -> TrigramModel
+makeTrigramItem ([a, b, c], count) = singleton a (singleton b (singleton c count))
 
 makeTrigramList :: [String] -> [[String]]
 makeTrigramList listAll@[a, b, c] = [listAll]
 makeTrigramList listAll = (take 3 listAll) : (makeTrigramList (tail listAll))
 
 searchAndCountWords :: [String] -> [[String]] -> Int
-searchAndCountWords key items = length $ Data.List.filter (==key) items
+searchAndCountWords key items = length $ List.filter (==key) items
 
-getTrigramFrequency :: [[String]] -> Map [String] Int
-getTrigramFrequency sList = fromList [(a, searchAndCountWords a sList) | a <- nub sList]
+getTrigramFrequency :: [[String]] -> [([String], Int)]
+getTrigramFrequency sList = [(a, searchAndCountWords a sList) | a <- nub sList]
 
