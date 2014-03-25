ecd23dcc29f2f55de3481bf2a4ef00aa78fb7cf4
haru2036 <haru2036@gmail.com>
2014-03-25 21:48:26 +0900

    Adding functions of lookup a next word (WIP)

--- Text/HanashiEngine/Generate.hs
+++ Text/HanashiEngine/Generate.hs
@@ -3,12 +3,16 @@ module Text.HanashiEngine.Generate
 )where
 
 import Text.HanashiEngine.Types
+import Data.Map as Map
 
 makeSentence :: TrigramModel -> [String] -> [String]
 makeSentence model single@[a] = (makeBigram model a) : single
-makeSentence model wipSentence = (makeTrigram model (take 2 wipSentence)) : wipSentence
+makeSentence model wipSentence = (selectHeadWords (lookupHeadWords model (take 2 wipSentence)) {- randomValue -}) : wipSentence
 
-makeTrigram :: TrigramModel -> [String] -> [String]
+lookupHeadWords :: TrigramModel -> [String] -> Maybe (Map String Int)
+lookupHeadWords model [first, second] = (Map.lookup first model) >>= \x-> Map.lookup second x
+
+selectHeadWords :: Map String Int -> Int -> String
 
 makeBigram :: TrigramModel -> String -> [String]
 
