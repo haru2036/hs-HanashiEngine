5ceadaf5687f26881899d1f20a6b0d88d95a1fdb
haru2036 <haru2036@gmail.com>
2014-03-27 21:20:49 +0900

    Added some function for lookup word of trigram's head

--- Text/HanashiEngine/Generate.hs
+++ Text/HanashiEngine/Generate.hs
@@ -2,17 +2,31 @@ module Text.HanashiEngine.Generate
 ( makeSentence
 )where
 
+import System.Random
 import Text.HanashiEngine.Types
 import Data.Map as Map
 
-makeSentence :: TrigramModel -> [String] -> [String]
-makeSentence model single@[a] = (makeBigram model a) : single
-makeSentence model wipSentence = (selectHeadWords (lookupHeadWords model (take 2 wipSentence)) {- randomValue -}) : wipSentence
+makeSentence :: TrigramModel -> [String] -> [Double] -> [String]
+makeSentence model single@[a] randomList = (makeBigram model a) : single
+makeSentence model wipSentence randomList = makeSentence model ((selectHeadWords (lookupHeadWords model (take 2 wipSentence)) (head randomList)) : wipSentence) (drop 1 randomList)
 
 lookupHeadWords :: TrigramModel -> [String] -> Maybe (Map String Int)
 lookupHeadWords model [first, second] = (Map.lookup first model) >>= \x-> Map.lookup second x
 
-selectHeadWords :: Map String Int -> Int -> String
+selectHeadWords :: Map String Int -> Double -> String
+selectHeadWords modelHead randomValue = (calcRandomPosition modelHead randomValue)
+
+calcRandomPosition :: [(String, Int)] -> Double -> Double
+calcRandomPosition = (1.0 / (sum (snd (unzip modelHead)))) * randomValue
+
+lookupWordOfRandomPosition :: [(String, Int)] -> Int -> Double -> String
+lookupWordOfRandomPosition modelHead sum position 
+  | sum < position = lookupWordOfRandomPosition (drop 1 modelHead) (sum + (snd (head modelHead))) position
+  | sum >= position = fst (head modelHead)
 
 makeBigram :: TrigramModel -> String -> [String]
 
+makeInfiniteRandoms :: IO [Double]
+makeInfiniteRandoms = do
+  gen <- getStdGen 
+  return $ randoms gen
