1b87c05145a143197332fd2424d6f1fe16538d0e
haru2036 <haru2036@gmail.com>
2014-03-29 22:22:33 +0900

    とりあえず現在の進捗状況をコミット

--- Text/HanashiEngine/Generate.hs
+++ Text/HanashiEngine/Generate.hs
@@ -6,25 +6,37 @@ import System.Random
 import Text.HanashiEngine.Types
 import Data.Map as Map
 
-makeSentence :: TrigramModel -> [String] -> [Double] -> [String]
-makeSentence model single@[a] randomList = (makeBigram model a) : single
-makeSentence model wipSentence randomList = makeSentence model ((selectHeadWords (lookupHeadWords model (take 2 wipSentence)) (head randomList)) : wipSentence) (drop 1 randomList)
+makeSentence :: TrigramModel -> [String] -> [Double] -> [String] 
+makeSentence model single@[a] randomList = makeSentence model ((makeBigram model a (head randomList)) ++ single) (drop 1 randomList)
+makeSentence model wipSentence randomList 
+  |"\n" /=  (head wipSentence) = makeSentence model ( maybe ("\n") (\x->x) ((lookupHeadWords model (take 2 wipSentence))  >>= (\x->return (selectHeadWords (toList x) (head randomList)))) : wipSentence) (drop 1 randomList)
+  |otherwise = wipSentence
 
 lookupHeadWords :: TrigramModel -> [String] -> Maybe (Map String Int)
 lookupHeadWords model [first, second] = (Map.lookup first model) >>= \x-> Map.lookup second x
 
-selectHeadWords :: Map String Int -> Double -> String
-selectHeadWords modelHead randomValue = (calcRandomPosition modelHead randomValue)
+selectHeadWords :: [(String, Int)] -> Double -> String
+selectHeadWords modelHead randomValue = lookupWordOfRandomPosition modelHead (truncate (calcModelHeadToRandomPosition modelHead randomValue))
 
-calcRandomPosition :: [(String, Int)] -> Double -> Double
-calcRandomPosition = (1.0 / (sum (snd (unzip modelHead)))) * randomValue
+calcModelHeadToRandomPosition :: [(String, Int)] -> Double -> Double
+calcModelHeadToRandomPosition modelHead randomValue = calcRandomPosition (sum (snd (unzip modelHead))) randomValue
 
-lookupWordOfRandomPosition :: [(String, Int)] -> Int -> Double -> String
-lookupWordOfRandomPosition modelHead sum position 
-  | sum < position = lookupWordOfRandomPosition (drop 1 modelHead) (sum + (snd (head modelHead))) position
+calcRandomPosition :: Int -> Double -> Double
+calcRandomPosition lengthSum randomValue = (1.0 / (fromIntegral lengthSum)) * randomValue
+
+lookupWordOfRandomPosition :: [(String, Int)] -> Int -> String
+lookupWordOfRandomPosition modelHead sum = lookupWordOfRandomPositionRec modelHead sum 0
+
+lookupWordOfRandomPositionRec :: [(String, Int)] -> Int -> Int -> String
+lookupWordOfRandomPositionRec modelHead sum position 
+  | sum < position = lookupWordOfRandomPositionRec (drop 1 modelHead) (sum + (snd (head modelHead))) position
   | sum >= position = fst (head modelHead)
 
-makeBigram :: TrigramModel -> String -> [String]
+makeBigram :: TrigramModel -> String -> Double -> [String]
+makeBigram model startWord randomValue = let 
+                                          keysList = keys $ Map.lookup startWord model 
+                                         in 
+                                           (truncate (calcRandomPosition (length keysList))) !! keysList
 
 makeInfiniteRandoms :: IO [Double]
 makeInfiniteRandoms = do
